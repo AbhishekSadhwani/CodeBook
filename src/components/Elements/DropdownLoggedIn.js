4d39ff58b5ff91c92d55e084343fe0ff217cbffe
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2024-07-12 16:47:02 +0530

    Updated Toastify

--- src/components/Elements/DropdownLoggedIn.js
+++ src/components/Elements/DropdownLoggedIn.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import { getUser, logout } from "../../Services";
 
 export const DropdownLoggedIn = ({showDropdown}) => {
